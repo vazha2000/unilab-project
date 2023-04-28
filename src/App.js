f9729a982c151612030127e03b382dac21573fb1
vazha2000 <vaja.siqtura2000@gmail.com>
2023-04-28 20:32:48 +0400

    Add home page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      koko
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
