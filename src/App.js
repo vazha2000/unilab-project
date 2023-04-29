32169e1f6d2a2dfbffa0aa596a8698d1f529e5a2
vazha2000 <vaja.siqtura2000@gmail.com>
2023-04-29 14:03:32 +0400

    Add signIn page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/sign-in' element={<SignIn />}/>
         </Routes>
       </BrowserRouter>
       
